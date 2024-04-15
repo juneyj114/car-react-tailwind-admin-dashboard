96e8e8f9cf389cafff84c2eb78fb80c6bcd00d2a
지우 <137967148+devjyu@users.noreply.github.com>
2024-04-15 10:15:20 +0900

    회원탈퇴 문구 수정

--- src/pages/Support/DeleteAccount.tsx
+++ src/pages/Support/DeleteAccount.tsx
@@ -33,20 +33,24 @@ const DeleteAccount = () => {
     return (
         <div className="text-xs lg:text-sm">
             <h1 className="text-lg font-bold mb-2">탈퇴 안내</h1>
-            <div className="mb-10">회원탈퇴를 신청하기 전에 안내 사항을 확인해주세요.</div>
+            <div className="mb-10">회원탈퇴를 신청하기 전 아래 안내 사항을 확인해주세요.</div>
             <ul>
                 <li className="mb-10">
                     <div className="font-bold mb-2">1. 탈퇴 아이디는 재사용 및 복구가 불가능합니다.</div>
                     <div className=""><span className="text-danger">탈퇴한 아이디는 본인과 타인 모두 재사용 및 복구가 불가</span>하오니 신중하게 선택하시기 바랍니다.</div>
                 </li>
                 <li className="mb-10">
-                    <div className="font-bold mb-2">2. 탈퇴 후 회원정보 및 이용기록은 모두 삭제됩니다.</div>
-                    <div>회원정보 및 이용기록은 모두 삭제되며, 삭제된 데이터는 복구되지 않습니다.</div>
+                    <div className="font-bold mb-2">2. 탈퇴 후 회원정보 및 이용기록은 아래와 같이 처립됩니다.</div>
+                    <div>삭제 데이터와 보관 데이터로 아래와 같이 분류됩니다.</div>
                     <div>삭제되는 내용을 확인하시고 필요한 데이터는 미리 백업을 해주세요.</div>
                     <div className="flex flex-col border-t border-solid mt-2">
                         <div className="flex flex-row items-center border-b">
-                            <div className="border-r text-right py-3 pl-3 pr-1 lg:pr-5">삭제되는 데이터</div>
-                            <div className="pl-1 lg:pl-5">세대 연결 해제, 차량번호, 입출차 내역, 계정 정보, 푸시 알림 내역</div>
+                            <div className="border-r text-right py-3 pl-3 pr-1 lg:pr-5">보관 데이터</div>
+                            <div className="pl-1 lg:pl-5">회원 세대차량번호, 세대차량 입/출차 내역, 차량 식별 이미지</div>
+                        </div>
+                        <div className="flex flex-row items-center border-b">
+                            <div className="border-r text-right py-3 pl-3 pr-1 lg:pr-5">삭제 데이터</div>
+                            <div className="pl-1 lg:pl-5">회원 알림 내역, 회원 App 토큰, 회원Q&A 내역, 등록 방문차량 입/출차 내역, 알림 수신 설정 내역</div>
                         </div>
                     </div>
                 </li>
