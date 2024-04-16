865193f94a9a59fc6dd44dd7d2176be134ea6446
지우 <137967148+devjyu@users.noreply.github.com>
2024-04-16 16:05:10 +0900

    로딩 스피너 변경

--- src/common/Loader/index.tsx
+++ src/common/Loader/index.tsx
@@ -1,7 +1,10 @@
+import { BarLoader } from "react-spinners";
+
 const Loader = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-white">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+      {/* <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div> */}
+      <BarLoader height={15} width={200} speedMultiplier={0.7} color="#2241F2" />
     </div>
   );
 };
