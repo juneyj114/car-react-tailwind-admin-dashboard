7234addfa2cf988acf08d8a752d9bd8122d13085
지우 <137967148+devjyu@users.noreply.github.com>
2024-04-22 17:51:21 +0900

    검색 시작날짜 디폴트 3일로 변경

--- src/state/atoms/startDateState.ts
+++ src/state/atoms/startDateState.ts
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 import { formatYMD } from "../../js/dateFormat";
 
 const startDate: Date = new Date();
-startDate.setDate(new Date().getDate() - 0);
+startDate.setDate(new Date().getDate() - 2);
 
 export const startDateState = atom<string>({
     key: "startDateState",
